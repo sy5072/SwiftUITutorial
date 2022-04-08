89d2c1a60785ad64635a289bb448c6e100b3c94b
Nick(Seungyun Kim) <sy5072@naver.com>
2022-04-08 11:56:01 +0900

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Seungyun Kim on 2022/04/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
